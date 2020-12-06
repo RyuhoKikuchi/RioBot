d9382c01e554abf29e3593ab5a2c0b3b911df024
RyuhoKikuchi <agemarudon1@gmail.com>
2020-12-06 10:41:32 +0000

    Additional font file was uploaded.

--- codes/photo_uploader.py
+++
@@ -1,9 +0,0 @@
-from instabot import Bot
-
-
-user_name = input("Enter your username: ")
-user_password = input("Enter the password: ")
-sentence = input("Enter your caption: ")
-bot = Bot()
-bot.login(username = user_name, password = user_password)
-bot.upload_photo("photo.jpg", caption = sentence)
